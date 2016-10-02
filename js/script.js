70db711bbdadab8516283e17ffc5566f162abfa0
kwboyd <kwboyd@live.unc.edukwboyd$ git config --global user.email kwboyd@live.unc.edu>
2016-10-02 17:20:01 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//amountScrolled = 310;
+//    $(window).scroll(function() {
+//            if ( $(window).scrollTop() > amountScrolled ) {
+//                    $('#name-container').hide();
+//            } else {
+//                    $('#name-container').show();
+//            }
+//    });
